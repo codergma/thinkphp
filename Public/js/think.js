e1ec100e52ea29653cd0740391b8a2270b4d8453
codergma <codergma@gmail.com>
2015-12-24 20:06:45 +0800

    nav

--- Public/js/think.js
+++ Public/js/think.js
@@ -1,14 +1,30 @@
 (function($){
-	window.Think = {
-        "ROOT"   : "__ROOT__",
-        "APP"    : "__APP__",
-        "PUBLIC" : "__PUBLIC__",
-        "DEEP"   : "{:C('URL_PATHINFO_DEPR')}",
-        "MODEL"  : ["{:C('URL_MODEL')}", "{:C('URL_CASE_INSENSITIVE')}", "{:C('URL_HTML_SUFFIX')}"],
-        "VAR"    : ["{:C('VAR_MODULE')}", "{:C('VAR_CONTROLLER')}", "{:C('VAR_ACTION')}"],
-        "CITYID" : "{$currArea['areaId']}",
-        "MALL_TITLE" : "{$CONF['mallName']}",
-        "SMS_VERFY"  : "{$CONF['smsVerfy']}"
-	}
+
+        window.ThinkPHP = {};
+        ThinkPHP.test = function (url){
+                var pattern = /(?:\d{4})-(?:\d{2}-(\d{2}))/;
+                var str = "2010-01-02 2010-01-02";
+                var result = str.match(pattern);
+                return result;
+        }
+        /**
+         * 解析URL
+         * @param  {string} url 被解析的URL
+         * @return {object}     解析后的数据
+         */
+         ThinkPHP.parse_url = function (url){
+                var parse = url.match(/^(?:([a-z]+):\/\/)?([\w-]+(?:\.[\w-]+)+)?(?::(\d+))?([\w-\/]+)?(?:\?((?:\w+=[^#&=\/]*)?(?:&\w+=[^#&=\/]*)*))?(?:#([\w-]+))?$/i);
+                parse || $.error("url不合法") && return null;
+                
+                return{
+                       "scheme" : parse[1],
+                       "host"   : parse[2], 
+                       "port"   : parse[3],
+                       "path"   : parse[4],
+                       "query"  : parse[5],
+                       "fragment":parse[6]
+                };
+         }
+
 
 })(jQuery);
